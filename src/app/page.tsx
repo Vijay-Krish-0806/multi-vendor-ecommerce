75753031429798e14a102d3a58999ca694e3e7d7
Vijay <vijay@gmail.com>
2025-09-15 18:08:56 +0530

    components UI  update customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button> Hello World</Button>
+      <Button variant={"elevated"} className="m-10">
+        Something
+      </Button>
+      <div>
+        <Input placeholder="something" />
+        <Progress value={50} />
+        <Textarea placeholder="something" />
+      </div>
     </div>
   );
 }
