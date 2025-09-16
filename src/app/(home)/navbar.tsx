5536ba8ee7d7e1f9292613707e703cf076379234
Vijay <vijay@gmail.com>
2025-09-16 20:00:14 +0530

    sidebar and footer complete

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -5,7 +5,10 @@ import { Poppins } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
+import { NavbarSidebar } from "./navbar-sidebar";
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -43,6 +46,7 @@ const navbarItems = [
 
 export const Navbar = () => {
   const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
@@ -51,6 +55,11 @@ export const Navbar = () => {
           Shopy
         </span>
       </Link>
+      <NavbarSidebar
+        items={navbarItems}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
 
       <div className="items-center gap-4 hidden lg:flex">
         {navbarItems.map((item) => (
@@ -77,6 +86,16 @@ export const Navbar = () => {
           className="border-l border-t-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 transition-colors hover:text-black text-lg"
         >
           <Link href={"/sign-up"}>Start Selling</Link>
+        </Button>
+      </div>
+
+      <div className="flex lg:hidden items-center justify-center">
+        <Button
+          variant={"ghost"}
+          className="size-12 border-transparent bg-white"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <MenuIcon />
         </Button>
       </div>
     </nav>
