4e65719233f7ce5b3a93fde48f0006306ab657e2
Vijay-Krish-0806 <vijaykrishnainampudi6@gmail.com>
2025-09-30 22:05:21 +0530

    Products

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,16 +1,32 @@
+import {
+  ProductList,
+  ProductListSkeleton,
+} from "@/modules/products/ui/components/product-list";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
-  params: Promise<{ category: string; subcategory: string }>;
+  params: Promise<{ subcategory: string }>;
 }
 
-const SubCategoryPage = async ({ params }: Props) => {
-  const { category, subcategory } = await params;
+const CategoryPage = async ({ params }: Props) => {
+  const { subcategory } = await params;
+  // const products = await caller.products.getMany();
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.products.getMany.queryOptions({ category: subcategory })
+  );
   return (
     <div>
-      Category:{category}
-      <br />
-      SubCategory:{subcategory}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<ProductListSkeleton />}>
+          <ProductList category={subcategory} />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 };
 
-export default SubCategoryPage;
+export default CategoryPage;
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,3 +1,4 @@
+import { Category, Product } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import type { Where } from "payload";
 import z from "zod";
@@ -11,29 +12,45 @@ export const productsRouter = createTRPCRouter({
     .query(async ({ ctx, input }) => {
       const where: Where = {};
       if (input.category) {
-        //   const categoriesData = await ctx.db.find({
-        //     collection: "categories",
-        //     limit: 1,
-        //     pagination: false,
-        //     where: {
-        //       slug: {
-        //         equals: input.category,
-        //       },
-        //     },
-        //   });
-        //   const category = categoriesData.docs[0];
-        // if (category) {
+        const categoriesData = await ctx.db.find({
+          collection: "categories",
+          limit: 1,
+          depth: 1,
+          pagination: false,
+          where: {
+            slug: {
+              equals: input.category,
+            },
+          },
+        });
+
+        const formattedData = categoriesData.docs.map((doc) => ({
+          ...doc,
+          subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            ...(doc as Category),
+          })),
+        }));
+
+        const subcategorySlugs = [];
+
+        const parentCategory = formattedData[0];
+
+        if (parentCategory) {
+          subcategorySlugs.push(
+            ...parentCategory.subcategories.map(
+              (subcategory) => subcategory.slug
+            )
+          );
+        }
         where["category.slug"] = {
-          equals: input.category,
+          in: [parentCategory.slug, ...subcategorySlugs],
         };
-        // }
       }
       const data = await ctx.db.find({
         collection: "products",
         depth: 1,
         where,
       });
-      console.log(data);
 
       return data;
     }),
